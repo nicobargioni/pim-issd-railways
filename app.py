1458fb732a7d9e698d4de2b09ad803b330354063
Nico <nicobargioni@gmail.com>
2025-12-16 15:16:56 -0300

    fix: actualizar servidores TURN a OpenRelay

--- app.py
+++ app.py
@@ -18,12 +18,26 @@ mp_face_mesh = mp.solutions.face_mesh
 mp_drawing = mp.solutions.drawing_utils
 mp_selfie_segmentation = mp.solutions.selfie_segmentation
 
-# Configuración RTC para WebRTC
+# Configuración RTC para WebRTC con TURN servers (OpenRelay - públicos y gratuitos)
 RTC_CONFIGURATION = RTCConfiguration(
     {"iceServers": [
         {"urls": ["stun:stun.l.google.com:19302"]},
         {"urls": ["stun:stun1.l.google.com:19302"]},
-        {"urls": ["stun:stun2.l.google.com:19302"]},
+        {
+            "urls": "turn:openrelay.metered.ca:80",
+            "username": "openrelayproject",
+            "credential": "openrelayproject",
+        },
+        {
+            "urls": "turn:openrelay.metered.ca:443",
+            "username": "openrelayproject",
+            "credential": "openrelayproject",
+        },
+        {
+            "urls": "turn:openrelay.metered.ca:443?transport=tcp",
+            "username": "openrelayproject",
+            "credential": "openrelayproject",
+        },
     ]}
 )
 
